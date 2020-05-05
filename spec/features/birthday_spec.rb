60bc86bd4039dace262210f2f18e4096db3c162b
Ed <lookatemail@gmail.com>
2020-05-05 11:12:50 +0100

    feature test to expect #days_till_bday left

--- spec/features/birthday_spec.rb
+++ spec/features/birthday_spec.rb
@@ -1,5 +1,5 @@
 feature 'birthday view' do
-  scenario 'should have ' do
+  scenario 'should have Happy Birthday greet' do
     allow_any_instance_of(Time).to receive(:day) { 5 }
     allow_any_instance_of(Time).to receive(:month) { 3 }
     visit('/')
@@ -8,5 +8,16 @@ feature 'birthday view' do
     select 'March', from: 'month'
     click_button 'Submit'
     expect(page).to have_content 'Happy Brthday Dave'
+  end
+
+  scenario 'should return days till Birthday' do
+    allow_any_instance_of(Time).to receive(:day) { 11 }
+    allow_any_instance_of(Time).to receive(:month) { 3 }
+    visit('/')
+    fill_in :name, with: 'Dave'
+    fill_in :day, with: 12
+    select 'April', from: 'month'
+    click_button 'Submit'
+    expect(page).to have_content '1 days and 1 month till your Birthday Dave'
   end
 end
